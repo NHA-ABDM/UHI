a38f700af1f9a6dadfca79d85d672c4890bb5700
National Health Authority <96580594+NHA-ABDM@users.noreply.github.com>
2022-05-01 01:14:50 +0530

    Initial commit - Beta Release
    Spring cloud gateway - WIP

---
+++ src/gateway/gateway/src/test/java/in/gov/abdm/uhi/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.gov.abdm.uhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
